abb94e4d6595def90c65095e48394b54d0c5ff4f
Kp1303 <kashishpundir1303@gmail.com>
2026-08-17 13:32:11 +0530

    Initial Jenkins pipeline project

---
+++ app.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print("Jenkins CI/CD Pipeline")
+    print("Build executed successfully!")
+
+    result = add(10, 20)
+    print("10 + 20 =", result)
